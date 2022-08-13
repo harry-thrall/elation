8c2f3ab62fbd52879b24c31854d0df647f122ee2
harry-thrall <harry.thrall@yahoo.co.uk>
2022-08-13 03:55:58 +0100

    Added slick slider

--- wp-content/themes/twentytwenty-child/functions.php
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -1,11 +1,17 @@
 <?php
     function ht_enqueue_styles() { 
       wp_enqueue_script('jquery');
-      wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
-      wp_enqueue_style( 'home-style', get_stylesheet_directory_uri() . '/css/home.css' );
       wp_enqueue_style( 'owl-style', get_stylesheet_directory_uri() . '/scss/owl.carousel.min.css' );
       wp_enqueue_script( 'owl-script', get_stylesheet_directory_uri() . '/js/owl.carousel.min.js' );
       wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js' );
+      wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+      wp_enqueue_style( 'home-style', get_stylesheet_directory_uri() . '/css/home.css' );
+      wp_enqueue_style( 'responsive-style', get_stylesheet_directory_uri() . '/css/responsive.css' );
+
+      wp_register_script('slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js', array('jquery'), '1.0.0'); // Custom scripts
+wp_enqueue_script('slick'); // Enqueue it!
+wp_register_script('slicknav', 'https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.10/jquery.slicknav.min.js', array('jquery'), '1.0.0'); // Custom scripts
+wp_enqueue_script('slicknav'); // Enqueue it!
     }
 
     add_action( 'wp_enqueue_scripts', 'ht_enqueue_styles' );
--- wp-content/themes/twentytwenty-child/js/custom.js
+++ wp-content/themes/twentytwenty-child/js/custom.js
@@ -8,23 +8,12 @@ jQuery(window).scroll(function () {
 
 jQuery(document).ready(function() {
 
-    jQuery('.owl-carousel.hero-slider').owlCarousel({
-         loop:true,
-         autoplay:true,
-         autoplaySpeed:100,
-         autoplayTimeout:7000,
-         autoplayHoverPause:true,
-         stagePadding:0,
-         dots:true,
-         responsiveClass:true,
-         responsive:{
- 
-         300:{
-         items:1,
-         },
- 
-         }
-     });
+    jQuery('.hero-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+    });
 
      jQuery('.owl-carousel.clients-slider').owlCarousel({
 		loop:true,
@@ -68,6 +57,7 @@ jQuery(document).ready(function() {
     console.log('changed');
     if(this.checked){
         jQuery('.nav-menu').addClass('opened');
+        jQuery('body').addClass('no-scroll');
     } else {
         closeMenu();
     }
@@ -79,8 +69,59 @@ closeMenu();
 
  function closeMenu(){
     jQuery('.nav-menu').removeClass('opened');
+    jQuery('body').removeClass('no-scroll');
     jQuery('.toggle').removeAttr('checked');
  }
 
 });
- 
+
+$slickGreen = false;
+    function greenSlider(){    
+        if(jQuery(window).width() < 1201){
+			console.log("small");
+            if(!$slickGreen){
+                jQuery(".product-slider").slick({
+                    dots: false,
+                    arrows: false,
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+					responsive: [
+						{
+							breakpoint: 1200,
+							settings: {
+								slidesToShow: 2,
+								slidesToScroll: 2,
+								infinite: true,
+								dots: true,
+							}
+						},
+						{
+							breakpoint: 800,
+							settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1,
+								infinite: true,
+								dots: true,
+							}
+						}
+					]
+                });
+                $slickGreen = true;
+            } else {
+				jQuery(".product-slider")[0].slick.refresh();
+			}
+        } else if(jQuery(window).width() > 1201){
+			console.log("bigger");
+            if($slickGreen){
+                jQuery('.product-slider').slick('unslick');
+                $slickGreen = false;
+            }
+        }
+    };
+
+    jQuery(document).ready(function(){
+        greenSlider();
+    });
+    jQuery(window).on('resize', function(){
+         greenSlider();
+    });
